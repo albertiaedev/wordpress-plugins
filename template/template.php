97a4bc7f826d44fcc2f80b87d58d25438abb436c
Alberto <albertiae.digital@gmail.com>
2023-02-22 20:16:42 -0400

    Create a basic template for a wordpress plugin

---
+++ template/template.php
@@ -0,0 +1,16 @@
+<?php
+	/*
+	* Plugin Name:
+	* Plugin URI:
+	* Description:
+	* Version:
+	* Author:
+	* Author URI:
+	* Text Domain:
+	* Domain Path:
+	* License:
+	* License URI:
+	*/
+
+	// THE CODE OF YOUR PLUGIN GOES HERE
+?>
